3699fcdbfec89c8ab4bb6993b50eac27cb3f0b89
Virus-Networks <168676321+Virus-Networks@users.noreply.github.com>
2024-08-04 22:14:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ from os import environ
 
 API_ID       = int(environ.get("API_ID", "19094598"))
 API_HASH     = environ.get("API_HASH", "479c00e863915ba2f8b90d8030ca4786")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "7329951541:AAHWqJNmIiq0GTpA3RNv56SIzPUU9qXh_5k")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "")
 DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://anmol:anmol@cluster0.eaq9icz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002114115777"))
 ADMIN        = int(environ.get("ADMIN", "1663603208"))
